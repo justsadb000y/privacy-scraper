3b24482681af356218e3e794e75686d43e3dd831
justsadb000y <sadb000y@proton.me>
2025-03-21 21:38:27 -0300

    (feat): new features
    - multithread
    - fast mp4 conversion
    - don't download duplicate files

--- privacy_scraper.py
+++ privacy_scraper.py
@@ -8,6 +8,7 @@ import shutil
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 if not shutil.which("ffmpeg"):
     raise Exception("FFmpeg não encontrado. Instale e adicione ao PATH: https://ffmpeg.org/download.html")
@@ -18,7 +19,7 @@ class PrivacyScraper:
     def __init__(self):
         self.scraper = cloudscraper.create_scraper(
             browser={
-                'browser': 'chrome',  # Simulando navegador real
+                'browser': 'chrome',
                 'platform': 'windows',
                 'desktop': True
             }
@@ -78,9 +79,15 @@ class PrivacyScraper:
             return []
 
     def get_total_media_count(self, profile_name):
+        headers_profile = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+            'Accept': '*/*',
+            'Connection': 'keep-alive',
+            "authorization": f"Bearer {self.token_v2}",
+        }
         url = f"https://privacy.com.br/profile/{profile_name}/Mosaico"
-        response = self.scraper.get(url)
-        print(f"Media count: {response.status_code}")
+        response = self.scraper.get(url, headers=headers_profile)
+        
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             total_match = soup.find('a', class_='filter-button selected')
@@ -92,6 +99,8 @@ class PrivacyScraper:
             videos = int(videos_match.text.split()[0]) if videos_match else 0
 
             return total, photos, videos
+        else:
+            print(f"Não foi possível obter quantidade mídias. Response code: {response.status_code}")
         return 0, 0, 0
 
 class MediaDownloader:
@@ -112,7 +121,7 @@ class MediaDownloader:
                         pbar.update(1)
                 return True
             else:
-                print(f"Falha ao baixar {url}: Status {response.status_code}. Uri {tokenContent}")
+                print(f"Falha ao baixar {url}: Status {response.status_code}.")
                 return False
         except Exception as e:
             print(f"Erro ao baixar o arquivo: {e}")
@@ -174,26 +183,50 @@ class MediaDownloader:
 
     def convert_m3u8_to_mp4(self, input_file, output_file):
         try:
-            print(f"Convertendo {input_file} para MP4...")
-            
+            print(f"Tentando converter {input_file} para MP4 com codec COPY (rápido)...")
+
             if not os.path.exists(input_file):
-                raise FileNotFoundError(f"Arquivo de entrada não encontrado: {input_file}")
+                raise FileNotFoundError(f"Arquivo não encontrado: {input_file}")
 
             os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-            (
-                ffmpeg
-                .input(input_file, allowed_extensions='ALL')
-                .output(output_file, 
-                        vcodec='libx264', 
-                        acodec='aac',
-                        loglevel='error')
-                .overwrite_output()
-                .run()
-            )
-            
-            print(f"Conversão concluída: {output_file}")
-            return True
+            try:
+                ffmpeg.input(input_file, allowed_extensions='ALL').output(
+                    output_file,
+                    vcodec='copy',
+                    acodec='copy',
+                    loglevel='error'
+                ).overwrite_output().run()
+
+                print(f"[SUCESSO] Conversão rápida concluída: {output_file}")
+                return True
+
+            except ffmpeg.Error as e:
+                print(f"[AVISO] Conversão rápida falhou: {e.stderr.decode() if e.stderr else 'Erro desconhecido'}")
+                print("Tentando conversão com reencode (mais lenta)...")
+
+                # Segunda tentativa (reencode com preset rápido)
+                ffmpeg.input(input_file, allowed_extensions='ALL').output(
+                    output_file,
+                    vcodec='libx264',
+                    acodec='aac',
+                    preset='ultrafast',
+                    crf=23,
+                    loglevel='error'
+                ).overwrite_output().run()
+
+                print(f"[SUCESSO] Conversão com reencode concluída: {output_file}")
+                return True
+
+        except ffmpeg.Error as e:
+            print("[ERRO CRÍTICO] Conversão com reencode também falhou:")
+            print(e.stderr.decode() if e.stderr else "Erro desconhecido")
+            return False
+
+        except Exception as e:
+            print(f"[ERRO GERAL] Problema durante a conversão: {str(e)}")
+            return False
+
             
         except ffmpeg.Error as e:
             print("Erro na conversão do vídeo:")
@@ -211,6 +244,16 @@ class MediaDownloader:
         except Exception as e:
             print(f"Erro ao remover arquivos temporários: {e}")
 
+def download_media_parallel(media_downloader, media_tasks, max_workers=os.getenv('WORKERS')):
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for task in media_tasks:
+            future = executor.submit(task['func'], *task['args'])
+            futures.append(future)
+        
+        for future in tqdm(as_completed(futures), total=len(futures), desc="Baixando mídias"):
+            future.result()
+
 def main():
     privacy_scraper = PrivacyScraper()
 
@@ -227,6 +270,7 @@ def main():
             media_type = input("Selecione o tipo de mídia para download (1 - Fotos, 2 - Vídeos, 3 - Ambos): ")
 
             media_downloader = MediaDownloader(privacy_scraper.scraper)
+
             for selected_profile_name in selected_profiles:
                 print(f"Processando perfil: {selected_profile_name}")
 
@@ -237,79 +281,98 @@ def main():
                 os.makedirs(f"./{selected_profile_name}/videos", exist_ok=True)
 
                 skip = 0
-                downloaded_photos = 0
-                downloaded_videos = 0
-                with tqdm(total=total_photos + total_videos, desc="Progresso total") as pbar:
-                    while True:
-                        unix_timestamp = int(time.time() * 1000)
-                        third_url = f"https://privacy.com.br/Profile?handler=PartialPosts&skip={skip}&take=10&nomePerfil={selected_profile_name}&filter=mosaico&_={unix_timestamp}"
-                        response = privacy_scraper.scraper.get(third_url)
+                media_tasks = []
 
-                        if response.status_code == 200:
-                            response_data = response.json()
-                            if not response_data.get("mosaicItems"):
-                                break
+                while True:
+                    unix_timestamp = int(time.time() * 1000)
+                    headers = {
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+                        'Accept': '*/*',
+                        'Connection': 'keep-alive',
+                        "authorization": f"Bearer {privacy_scraper.token_v2}",
+                    }
+                    third_url = f"https://privacy.com.br/Profile?handler=PartialPosts&skip={skip}&take=10&nomePerfil={selected_profile_name}&filter=mosaico&_={unix_timestamp}"
+                    response = privacy_scraper.scraper.get(third_url, headers=headers)
 
-                            for item in response_data.get("mosaicItems", []):
-                                for file in item.get("files", []):
-                                    if not file["isLocked"]:
-                                        file_type = file["type"]
-                                        file_url = file["url"]
-
-                                        if file_type == "image" and media_type in ["1", "3"]:
-                                            filename = f"./{selected_profile_name}/fotos/{file['mediaId']}.jpg"
-                                            if media_downloader.download_file(file_url, filename, '', pbar):
-                                                downloaded_photos += 1
-                                        elif file_type == "video" and media_type in ["2", "3"]:
-                                            base_path = f"./{selected_profile_name}/videos/{file['mediaId']}_temp"
-                                            os.makedirs(base_path, exist_ok=True)
-                                            main_m3u8_filename = os.path.join(base_path, "main.m3u8")
-                                            media_token_header = {
-                                                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
-                                                'Accept': '*/*',
-                                                'Connection': 'keep-alive',
-                                                "authorization": f"Bearer {privacy_scraper.token_v2}",
-                                            }
-                                            fileId = file_url.split("/hls/")[0].split("/")[-1]
-                                            payload = {
-                                                'exp': 3600,
-                                                'file_id': fileId
-                                            }
-                                            token_url = "https://service.privacy.com.br/media/video/token"
-
-                                            response = privacy_scraper.scraper.post(token_url, json=payload, headers=media_token_header)
-                                            responseContent = response.json()
-                                            content = responseContent.get('content')
-
-                                            if media_downloader.download_file(file_url, main_m3u8_filename, content):
-                                                with open(main_m3u8_filename, 'r', encoding='utf-8') as f:
-                                                    main_m3u8_content = f.read()
-
-                                                best_quality_url = media_downloader.get_best_quality_m3u8(file_url, main_m3u8_content)
-                                                if best_quality_url:
-                                                    best_m3u8_filename = media_downloader.process_m3u8(best_quality_url, base_path, content)
-                                                    
-                                                    if best_m3u8_filename and os.path.exists(best_m3u8_filename):
-                                                        output_filename = f"./{selected_profile_name}/videos/{file['mediaId']}.mp4"
-                                                        print(f"Tentando converter: {best_m3u8_filename} -> {output_filename}")
-                                                        
-                                                        if media_downloader.convert_m3u8_to_mp4(best_m3u8_filename, output_filename):
-                                                            downloaded_videos += 1
-                                                            pbar.update(1)
-
-                                            media_downloader.clean_temp_files(base_path)
-                        else:
-                            print(f"Falha ao buscar mosaico: {response.status_code}")
-
-                        skip += 10
-                        if skip >= total:
+                    if response.status_code == 200:
+                        response_data = response.json()
+                        if not response_data.get("mosaicItems"):
                             break
 
-                print(f"Download concluído para {selected_profile_name}. Fotos: {downloaded_photos}, Vídeos: {downloaded_videos}")
+                        for item in response_data.get("mosaicItems", []):
+                            for file in item.get("files", []):
+                                if not file["isLocked"]:
+                                    file_type = file["type"]
+                                    file_url = file["url"]
+
+                                    if file_type == "image" and media_type in ["1", "3"]:
+                                        filename = f"./{selected_profile_name}/fotos/{file['mediaId']}.jpg"
+                                        
+                                        if not os.path.exists(filename):
+                                            media_tasks.append({
+                                                'func': media_downloader.download_file,
+                                                'args': (file_url, filename, '')
+                                            })
+                                        else:
+                                            print(f"Foto já baixada anteriormente: {filename}")
+
+                                    elif file_type == "video" and media_type in ["2", "3"]:
+                                        output_filename = f"./{selected_profile_name}/videos/{file['mediaId']}.mp4"
+                                        
+                                        if not os.path.exists(output_filename):
+                                            media_tasks.append({
+                                                'func': download_and_process_video,
+                                                'args': (privacy_scraper, media_downloader, selected_profile_name, file)
+                                            })
+                                        else:
+                                            print(f"Vídeo já baixado anteriormente: {output_filename}")
+                    else:
+                        print(f"Falha ao buscar mosaico: {response.status_code}")
+                        break
+
+                    skip += 10
+                    if skip >= total:
+                        break
+
+                # Realiza o download paralelo
+                download_media_parallel(media_downloader, media_tasks, max_workers=5)
+
         else:
             print("Nenhum perfil encontrado.")
     else:
         print("Falha no login.")
+
+def download_and_process_video(privacy_scraper, media_downloader, profile_name, file):
+    base_path = f"./{profile_name}/videos/{file['mediaId']}_temp"
+    os.makedirs(base_path, exist_ok=True)
+    main_m3u8_filename = os.path.join(base_path, "main.m3u8")
+
+    media_token_header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'Accept': '*/*',
+        'Connection': 'keep-alive',
+        "authorization": f"Bearer {privacy_scraper.token_v2}",
+    }
+    fileId = file["url"].split("/hls/")[0].split("/")[-1]
+    payload = {'exp': 3600, 'file_id': fileId}
+
+    token_url = "https://service.privacy.com.br/media/video/token"
+    response = privacy_scraper.scraper.post(token_url, json=payload, headers=media_token_header)
+    content = response.json().get('content')
+
+    if media_downloader.download_file(file["url"], main_m3u8_filename, content):
+        with open(main_m3u8_filename, 'r', encoding='utf-8') as f:
+            main_m3u8_content = f.read()
+
+        best_quality_url = media_downloader.get_best_quality_m3u8(file["url"], main_m3u8_content)
+        if best_quality_url:
+            best_m3u8_filename = media_downloader.process_m3u8(best_quality_url, base_path, content)
+
+            if best_m3u8_filename and os.path.exists(best_m3u8_filename):
+                output_filename = f"./{profile_name}/videos/{file['mediaId']}.mp4"
+                media_downloader.convert_m3u8_to_mp4(best_m3u8_filename, output_filename)
+
+    media_downloader.clean_temp_files(base_path)
 
 if __name__ == "__main__":
     main()
